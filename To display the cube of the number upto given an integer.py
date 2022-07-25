1b937a0d9443422c2ceb8fb98ce15b2a355daa15
The-jinnn <gowtham141097@gmail.com>
2022-07-26 00:30:30 +0530

    now

--- To display the cube of the number upto given an integer.py
+++ To display the cube of the number upto given an integer.py
@@ -3,7 +3,7 @@ a=int(input("Input the number of terms : "))
 cube=0
 
 for i in range(1,a+1):
-    
+
     cube=i**3
 
-    print(f"Number is : {i} and the cube of {i} is {cube}")
+    print(f"Number is : {i} and the cube of {i} is: {cube}")
