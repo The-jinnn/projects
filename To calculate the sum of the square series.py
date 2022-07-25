16f8a9df7eec1b6a7a06e57d5080c66d9139616f
The-jinnn <gowtham141097@gmail.com>
2022-07-25 19:05:12 +0530

    now

---
+++ To calculate the sum of the square series.py
@@ -0,0 +1,14 @@
+a=int(input("Input the value for nth term: "))
+
+c=0
+sum=0
+
+for i in range (1,a+1):
+   
+    c=(i*i)
+
+    print(f"{i}*{i}= {c}")
+    
+    sum=sum+c
+
+print(f"The sum of the above series is: {sum}")
