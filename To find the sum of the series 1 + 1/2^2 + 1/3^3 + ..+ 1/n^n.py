8e3fb6b2870900fc954287cf642fd79dc04d1659
The-jinnn <gowtham141097@gmail.com>
2022-07-25 18:23:49 +0530

    now

--- To find the sum of the series 1 + 1/2^2 + 1/3^3 + ..+ 1/n^n.py
+++ To find the sum of the series 1 + 1/2^2 + 1/3^3 + ..+ 1/n^n.py
@@ -1,1 +1,11 @@
-a=int(input(""))
+a=int(input("Input the value for nth term: "))
+
+c=0
+
+sum=0
+
+for i in range (1,a+1):
+    c=(1/(i**i))
+    print(f"1/{i}^{i}= {c}")
+    sum=sum+c
+print(f"The sum of the above series is: {sum}")
--- prime number.py
+++ prime number.py
@@ -1,9 +1,12 @@
 a=int(input("Input a number to check prime or not: "))
 found= False
-for i in range (2,a):
-    if(a%i==0):
-        found= True
-if(found==True):
+if (a==0)or(a==1):
     print("The entered number is not a prime numer")
 else:
-    print("The entered number is a prime number")
+    for i in range (2,a):
+        if(a%i==0):
+            found= True
+    if(found==True):
+        print("The entered number is not a prime numer")
+    else:
+        print("The entered number is a prime number")
