6eed47fb037cec97fba2d5d528630f888f1214be
The-jinnn <gowtham141097@gmail.com>
2022-07-26 11:21:03 +0530

    reverse

---
+++ To display the number in reverse order.py
@@ -0,0 +1,11 @@
+a=input("Input a number: ")
+
+rev=0
+
+for i in range (1,len(a)+1):
+    
+    rem = int(a) % 10  
+    rev = (rev * 10) + rem 
+    a = int(a) // 10  
+
+print("The number in reverse order is :",rev)
