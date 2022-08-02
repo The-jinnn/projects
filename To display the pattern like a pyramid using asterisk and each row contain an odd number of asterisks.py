f8e8d674d389ced2c33cb173b18aa392c590aed8
goutham <99198589+The-jinnn@users.noreply.github.com>
2022-08-02 14:33:20 +0530

    pyramid odd number of asterisks

---
+++ To display the pattern like a pyramid using asterisk and each row contain an odd number of asterisks.py
@@ -0,0 +1,9 @@
+a=int(input("Input number of rows: "))
+
+for i in range (1,a*2):
+    if(i%2!=0):
+        for j in range(a*2,i,-1):
+            print(end=' ')
+        for s in range(0,i):
+            print('*',end=' ')
+        print("\n")
