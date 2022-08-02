c5e4c5d1f17774c1107f478683a33ea8775a1178
goutham <99198589+The-jinnn@users.noreply.github.com>
2022-08-02 16:36:48 +0530

    reverse pyramid highest columns

---
+++ To display the pattern using digits with right justified and the highest columns appears in first row.py
@@ -0,0 +1,9 @@
+a=int(input("Input number of rows: "))
+
+for i in range(a,0,-1):
+    for j in range(0,a-i):
+        print(end=' ')
+    for k in range(1,i+1):
+        print(k,end=' ')
+    print("\n")
+    
