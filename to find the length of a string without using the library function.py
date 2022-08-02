be8179aa43575a6f34051bdf828672bc40b132f9
goutham <99198589+The-jinnn@users.noreply.github.com>
2022-08-02 10:31:56 +0530

    The length of a string

---
+++ to find the length of a string without using the library function.py
@@ -0,0 +1,10 @@
+a=input("Input a string : ")
+
+count = 0
+
+for i in range (0,len(a)):
+
+    count = count + 1
+
+print(f'''The string contains {count} number of characters.
+So, the length of the string {a} is : {count}''')
