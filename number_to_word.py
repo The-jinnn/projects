ebd68cf0eaa90c7e37688e242051ea106bbdda85
goutham <99198589+The-jinnn@users.noreply.github.com>
2022-08-11 14:21:57 +0530

    q.no : 60

---
+++ number_to_word.py
@@ -0,0 +1,23 @@
+a = input("Input any number: ")
+
+for i in range (len(a)):
+    if a[i]=='0':
+        print('Zero',end=' ')
+    elif a[i]=='1':
+        print('One',end=' ')
+    elif a[i]=='2':
+        print('Two',end=' ')            
+    elif a[i]=='3':
+        print('Three',end=' ')
+    elif a[i]=='4':
+        print('Four',end=' ')        
+    elif a[i]=='5':
+        print('Five',end=' ')
+    elif a[i]=='6':
+        print('Six',end=' ')
+    elif a[i]=='7':
+        print('Seven',end=' ')                
+    elif a[i]=='8':
+        print('Eight',end=' ')
+    elif a[i]=='9':
+        print('Nine',end=' ')
